0ab9a7301619716a3ec7ac6738ba2f3139884325
Lainezz <diegolinaresmojacar@gmail.com>
2021-12-30 17:54:35 +0100

    Modificaciones, comentarios y funcionalidad nueva

--- logica.js
+++ logica.js
@@ -1,54 +1,78 @@
 
-//Variable global que contendrá todos los personajes del LoL. Se seteará al cargar
-//la página, al realizar el primer fetch
+/*
+Variable global que contendrá todos los personajes del LoL. Se seteará al cargar
+la página, al realizar el primer fetch
+*/
 var personajes;
 
-
-function doFetch(){
-    fetch('http://ddragon.leagueoflegends.com/cdn/11.24.1/data/es_ES/champion.json')
-            .then(response => response.json())
-            .then(data => personajes=data.data);
+/*
+Función que inicializa la página. Realiza una petición a la API del Lol para obtener los personajes, rellena el select, etc etc.
+*/
+async function inicializarPagina() {
+    await fetch('http://ddragon.leagueoflegends.com/cdn/11.24.1/data/es_ES/champion.json')
+        .then(response => response.json())
+        .then(data => personajes = data.data);
+    populateSelect();
 }
 
-function doFetchImage(namePersonaje){
-    
-    console.log("En Fetch de Imagen con psj: "+namePersonaje);
-    fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${namePersonaje}_0.jpg`)
+/*
+Función que realiza un fetch para coger la imagen del personaje indicado
+*/
+async function doFetchImage(namePersonaje) {
+
+    let imageObjectURL;
+    await fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${namePersonaje}_0.jpg`)
         .then(response => response.blob())
-        .then(imageBlob => {
-            const imageObjectURL = URL.createObjectURL(imageBlob);
-            crearDiv(imageObjectURL);
-        });
+        .then(imageBlob => imageObjectURL = URL.createObjectURL(imageBlob));
+    
+    crearDiv(imageObjectURL);
 }
 
-//Función para manejar el submit del formulario
-function handleForm(event){
+/*
+Función para manejar el submit del formulario
+*/
+function handleForm(event) {
 
     event.preventDefault();
+
+    const inputName = event.target.name.value;
+    const optionName = event.target.selectPsjs.value;
+    let nombrePersonaje = "";
+
+    if(inputName !== ""){
+        nombrePersonaje = inputName;
+    }else if(optionName !== ""){
+        nombrePersonaje = optionName;
+    }else{
+        console.log("Error");
+    }
+
     Object.entries(personajes).map(psj => {
-        if(psj[1].name === event.target.name.value){
-            console.log(psj[1].name);
+        if (psj[1].name === nombrePersonaje) {
             doFetchImage(psj[1].name);
         }
     });
 }
 
-//Función para resetear los divs de los personajes. Para que no se acumulen uno 
-//al lado del otro.
-function resetPage(){
+/*
+Función para resetear los divs de los personajes. Para que no se acumulen uno 
+al lado del otro.
+*/
+function resetPage() {
     var divPsjs = document.querySelector("#psjs div");
-    console.log(divPsjs);
-    if(divPsjs!=null){
+    if (divPsjs != null) {
         divPsjs.remove();
     }
 }
 
-
-function crearDiv(urlImg){
+/*
+Función para crear un Div que contendrá la imagen de un personaje
+*/
+function crearDiv(urlImg) {
     resetPage();
-    
+
     var divPsjs = document.querySelector("#psjs");
-    
+
     var divCaja = document.createElement("div");
 
     var elemImg = document.createElement("img");
@@ -58,5 +82,19 @@ function crearDiv(urlImg){
     divPsjs.appendChild(divCaja);
 }
 
+/*
+Función para rellenar el select del formulario principal que nos sirve para poder seleccionar un personaje
+*/
+function populateSelect() {
+    var selectPsjs = document.querySelector("#selectPsjs");
+    Object.entries(personajes).map(psj => {
+        var option = document.createElement("option");
+        option.setAttribute("id", psj[1].name);
+        option.setAttribute("name", psj[1].name);
+        option.setAttribute("value", psj[1].name);
+        option.innerText = psj[1].name;
+        selectPsjs.appendChild(option);
+    });
+}
 
-window.onload = doFetch();
+window.onload = inicializarPagina();
