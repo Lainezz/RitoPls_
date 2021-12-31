9cdc11395547135f9e8f96e3bb4c9b85cdf94136
Lainezz <diegolinaresmojacar@gmail.com>
2021-12-31 15:01:33 +0100

    something

--- logica.js
+++ logica.js
@@ -17,15 +17,25 @@ async function inicializarPagina() {
 }
 
 /*
-* Función que realiza un fetch para coger la imagen del personaje indicado
+* Función que realiza un fetch para obtener de la API del LOL la imagen del personaje indicado
 */
-async function doFetchImage(namePersonaje) {
+/*
+async function doFetchImage(nombrePersonaje) {
 
-    let imageObjectURL = `./sprites/campeonesMD/${namePersonaje}_0.jpg`;
-    /*await fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${namePersonaje}_0.jpg`)
+    let imageObjectURL = "";
+    await fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${nombrePersonaje}_0.jpg`)
         .then(response => response.blob())
         .then(imageBlob => imageObjectURL = URL.createObjectURL(imageBlob));
-    */
+    crearDiv(imageObjectURL);
+}
+*/
+
+/*
+* Función que obtiene la imagen del personaje indicado
+*/
+function obtenerImagen(nombrePersonaje) {
+
+    let imageObjectURL = `./sprites/campeonesMD/${nombrePersonaje}_0.jpg`;
     crearDiv(imageObjectURL);
 }
 
@@ -35,25 +45,48 @@ async function doFetchImage(namePersonaje) {
 function handleForm(event) {
 
     event.preventDefault();
+    resetPage();
 
     const inputName = event.target.name.value;
     const optionName = event.target.selectPsjs.value;
     const optionRol = event.target.selectRol.value;
     let nombrePersonaje = "";
+    let rolPersonaje = "";
 
     if(inputName !== ""){
         nombrePersonaje = inputName;
+        seleccionarPersonaje(nombrePersonaje);
     }else if(optionName !== ""){
         nombrePersonaje = optionName;
+        seleccionarPersonaje(nombrePersonaje);
     }else if(optionRol !== ""){
-        console.log("Error");
+        rolPersonaje = optionRol;
+        seleccionarRol(rolPersonaje);
     }
+    
+}
 
+/*
+* Función que imprime todos los personajes asociados a un rol determinado
+*/
+function seleccionarRol(rolPersonaje){
+    Object.entries(personajes).map(psj => {
+        if (psj[1].tags[0] === rolPersonaje) {
+            let nombrePersonaje = psj[1].name;
+            const urlImg = `./sprites/campeonesMD/${nombrePersonaje}_0.jpg`;
+            crearDiv(urlImg, psj[1]);
+        }
+    });
+}
+
+/*
+* Función que imprime un personaje en concreto dado
+*/
+function seleccionarPersonaje(nombrePersonaje){
     Object.entries(personajes).map(psj => {
         if (psj[1].name === nombrePersonaje) {
             const urlImg = `./sprites/campeonesMD/${nombrePersonaje}_0.jpg`;
-            crearDiv(urlImg);
-            //doFetchImage(psj[1].name);
+            crearDiv(urlImg, psj[1]);
         }
     });
 }
@@ -63,24 +96,66 @@ function handleForm(event) {
 * al lado del otro.
 */
 function resetPage() {
-    var divPsjs = document.querySelector("#psjs div");
-    if (divPsjs != null) {
-        divPsjs.remove();
+    var divPsjs = document.querySelector("#psjs");
+    
+    if (divPsjs !== null) {
+        while(divPsjs.firstChild){
+            divPsjs.removeChild(divPsjs.lastChild);
+        }
     }
 }
 
 /*
 * Función para crear un Div que contendrá la imagen de un personaje
 */
-function crearDiv(urlImg) {
-    resetPage();
-
-    var divPsjs = document.querySelector("#psjs");
-    var divCaja = document.createElement("div");
-    var elemImg = document.createElement("img");
+function crearDiv(urlImg, personaje) {
+    let divPsjs = document.querySelector("#psjs");
+    let divCaja = document.createElement("div");
+    let elemImg = document.createElement("img");
+    let divInfo = document.createElement("div");
+    
+    
     elemImg.setAttribute("src", urlImg);
     divCaja.appendChild(elemImg);
+    
+    let elemPNombre = document.createElement("p");
+    elemPNombre.appendChild(document.createTextNode(personaje.name));
+    divInfo.appendChild(elemPNombre);
+
+
+
+    let inputJoke = document.createElement("input");
+    inputJoke.setAttribute("type", "button");
+    inputJoke.classList.add("bolaVerde");
+    inputJoke.setAttribute("id", personaje.name);
+    inputJoke.setAttribute("onclick", "playJoke(event)");
+    divInfo.appendChild(inputJoke);
+
+    let inputRisa = document.createElement("input");
+    inputRisa.setAttribute("type", "button");
+    inputRisa.classList.add("bolaRoja");
+    inputRisa.setAttribute("id", personaje.name);
+    inputRisa.setAttribute("onclick", "playLaugh(event)");
+    divInfo.appendChild(inputRisa);
+
+    divInfo.classList.add("caja2")
+    divCaja.appendChild(divInfo);
+    
+    divCaja.classList.add("col-sm-6");
+    divCaja.classList.add("col-lg-4");
+    divCaja.classList.add("caja");
+    divCaja.classList.add("g-3");
     divPsjs.appendChild(divCaja);
+}
+
+function playJoke(event){
+    console.log(event.target.id);
+    new Audio(`./sounds/jokes/${event.target.id}.joke.wav`).play();
+}
+
+function playLaugh(event){
+    console.log(event.target.id);
+    new Audio(`./sounds/laugh/${event.target.id}.laugh1.wav`).play();
 }
 
 /*
